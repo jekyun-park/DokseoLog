79f6e1537f1e8eb90bafd6905886110e24da44f6
parkjekyun@gmail.com <parkjekyun@gmail.com>
2023-07-25 00:28:37 +0900

    :tada: Init: #1 - 프로젝트 생성 및 스토리보드 제거
    프로젝트를 생성하고 스토리보드를 제거하였습니다.

---
+++ Bookie/Bookie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bookie
+//
+//  Created by 박제균 on 2023/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
