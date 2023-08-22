e25dc504beac47551cbbdbd7450ffdfd80b385a8
parkjekyun@gmail.com <parkjekyun@gmail.com>
2023-08-22 15:42:56 +0900

    :bento: Add: #5 - 추가한 컬러를 사용하기 위한 UIColor Extension 작성
    Assets에 추가한 컬러를 UIColor로 사용하기 위해 Extension을 작성해 두었습니다.

--- Bookie/Bookie/Extensions/UIColor+.swift
+++ Bookie/Bookie/Extensions/UIColor+.swift
@@ -5,4 +5,11 @@
 //  Created by 박제균 on 2023/08/22.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+
+  static let bkBackgroundColor = UIColor(named: "bkBackgroundColor")
+  static let bkTabBarBackgroundColor = UIColor(named: "bkTabBarBackgroundColor")
+
+}
