51c18aafa34d3aa1349f8ff140240dd700fcd2a4
jegyun <parkjekyun@gmail.com>
2023-11-03 15:47:53 +0900

    :label: Update: #9 - BKError에 케이스를 추가하였습니다.
    네트워크 통신중 발생하는 에러 케이스를 열거형에 추가하였습니다.

--- Bookie/Bookie/Utilities/BKError.swift
+++ Bookie/Bookie/Utilities/BKError.swift
@@ -8,5 +8,9 @@
 import Foundation
 
 enum BKError: String, Error {
-  case invalidAPIKey = "API 키가 잘못되었습니다. 개발자에게 문의해주세요."
+  case invalidAPIKey = "API 키가 잘못되었습니다. 개발자에게 문의하거나 다시 시도해 주세요."
+  case invalidURL = "잘못된 URL입니다. 개발자에게 문의하거나 다시 시도해 주세요."
+  case invalidResponse = "잘못된 응답입니다. 개발자에게 문의하거나 다시 시도해 주세요."
+  case invalidData = "서버로부터 받은 데이터에 오류가 있습니다. 개발자에게 문의하거나 다시 시도해 주세요."
+  case unableToComplete = "데이터 요청에 실패했습니다. 개발자에게 문의하거나 다시 시도해 주세요."
 }
