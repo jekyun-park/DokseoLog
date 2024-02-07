40c8ac3547e722c21fd8c90d519b060a19d10aa8
jegyun <parkjekyun@gmail.com>
2024-02-07 15:18:19 +0900

    :sparkles: Feat: #33 - ViewController에서 Custom Alert을 띄우는 Extension 추가
    Custom Alert을 띄울 수 있도록 Extension을 통해 UIViewController의 메서드를 추가하였습니다.

--- Bookie/Bookie/Extensions/UIViewController+.swift
+++ Bookie/Bookie/Extensions/UIViewController+.swift
@@ -1,8 +1,19 @@
 //
-//  UIViewController.swift
+//  UIViewController+.swift
 //  Bookie
 //
 //  Created by 박제균 on 2/6/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+
+  func presentBKAlert(title: String, message: String, buttonTitle: String, action: @escaping () -> Void) {
+    let alertViewController = BKAlertViewController(title: title, message: message, buttonTitle: buttonTitle, action: action)
+    alertViewController.modalPresentationStyle = .overFullScreen
+    alertViewController.modalTransitionStyle = .crossDissolve
+    self.present(alertViewController, animated: true)
+  }
+
+}
