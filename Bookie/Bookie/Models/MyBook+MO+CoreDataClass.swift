49b2863cd4f65e0457795fb17ab350dfee6731d8
jegyun <parkjekyun@gmail.com>
2024-02-20 10:44:34 +0900

    :recycle: Refactor: #35 - 에러 케이스 추가
    이미 저장된 데이터에 대한 에러 케이스를 추가합니다.

---
+++ Bookie/Bookie/Models/MyBook+MO+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  MyBook+MO+CoreDataClass.swift
+//  Bookie
+//
+//  Created by 박제균 on 2/16/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(MyBook+MO)
+public class MyBook+MO: NSManagedObject {
+
+}
