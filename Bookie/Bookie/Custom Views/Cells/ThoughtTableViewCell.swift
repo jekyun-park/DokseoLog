8f14fc0b176020ab65e6ea23138cb0b4dfe4460a
jegyun <parkjekyun@gmail.com>
2024-02-28 00:03:35 +0900

    :sparkles: Feat: #37 - 문장, 생각 기록 기능 추가
    문장 및 생각을 기록하는 기능을 추가하였습니다.

--- Bookie/Bookie/Custom Views/Cells/ThoughtTableViewCell.swift
+++ Bookie/Bookie/Custom Views/Cells/ThoughtTableViewCell.swift
@@ -9,15 +9,51 @@ import UIKit
 
 class ThoughtTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+  static let reuseID = "ThoughtTableViewCell"
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+  private lazy var thoughtLabel: BKBodyLabel = {
+    let label = BKBodyLabel(textAlignment: .left, fontSize: 15, fontWeight: .regular)
+    label.lineBreakMode = .byCharWrapping
+    return label
+  }()
 
-        // Configure the view for the selected state
-    }
+  private lazy var dateLabel: BKTitleLabel = {
+    let label = BKTitleLabel(textAlignment: .left, fontSize: 15, fontWeight: .medium)
+    return label
+  }()
+
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    setupUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func setupUI() {
+    let padding: CGFloat = 20
+
+    self.selectionStyle = .none
+    self.backgroundColor = .bkBackgroundColor
+    self.contentView.addSubviews(thoughtLabel, dateLabel)
+
+    NSLayoutConstraint.activate([
+      dateLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+      dateLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+      dateLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+      dateLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+
+      thoughtLabel.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: padding),
+      thoughtLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+      thoughtLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+      thoughtLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -padding),
+    ])
+  }
+
+  func setContent(thought: Thought) {
+    self.thoughtLabel.text = thought.memo
+    self.dateLabel.text = thought.createdAt.formatted()
+  }
 
 }
