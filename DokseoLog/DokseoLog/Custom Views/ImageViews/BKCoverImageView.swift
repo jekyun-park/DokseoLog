7c2e743c076b5611b88d44a67587b991f00a2296
jegyun <parkjekyun@gmail.com>
2024-03-06 16:03:16 +0900

    :sparkles: Feat: #41 - 이미지 로딩시 Indicator 처리
    이미지가 로딩중인 동안 UIActivityIndicatorView를 사용하여 불편함이 없도록 하였습니다.

--- DokseoLog/DokseoLog/Custom Views/ImageViews/BKCoverImageView.swift
+++ DokseoLog/DokseoLog/Custom Views/ImageViews/BKCoverImageView.swift
@@ -10,6 +10,11 @@ import UIKit
 class BKCoverImageView: UIImageView {
 
   // MARK: Lifecycle
+  private let indicatorView: UIActivityIndicatorView = {
+    let indicatorView = UIActivityIndicatorView(style: .large)
+    indicatorView.hidesWhenStopped = true
+    return indicatorView
+  }()
 
   override init(frame: CGRect) {
     super.init(frame: frame)
@@ -22,27 +27,30 @@ class BKCoverImageView: UIImageView {
 
   // MARK: Internal
 
-  let placeholderImage = Images.placeholderBookImage
-
   func downloadImage(fromURL urlString: String?) {
+
+    indicatorView.startAnimating()
     guard let url = urlString else {
-      DispatchQueue.main.async { self.image = self.placeholderImage }
       return
     }
 
     NetworkManager.shared.downloadImage(from: url, completion: { [weak self] image in
       guard let self else { return }
-      DispatchQueue.main.async { self.image = image }
+      DispatchQueue.main.async {
+        self.image = image
+        self.indicatorView.stopAnimating()
+      }
     })
   }
 
   // MARK: Private
 
   private func configureUI() {
+    self.addSubview(indicatorView)
     translatesAutoresizingMaskIntoConstraints = false
+    indicatorView.center = self.center
     layer.cornerRadius = 10
     clipsToBounds = true
-    image = placeholderImage
     contentMode = .scaleToFill
   }
 
