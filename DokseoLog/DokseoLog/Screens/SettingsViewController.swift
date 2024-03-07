20d954d4c9cbfc7567295cefcea96d22955425b7
jegyun <parkjekyun@gmail.com>
2024-03-08 00:30:04 +0900

    :sparkles: Feat: #43 - 세팅 탭에 앱 버전 표기
    세팅 탭에서 앱 버전을 표기하였습니다.

--- DokseoLog/DokseoLog/Screens/SettingsViewController.swift
+++ DokseoLog/DokseoLog/Screens/SettingsViewController.swift
@@ -9,10 +9,59 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+  let tableView = UITableView()
+
   override func viewDidLoad() {
     super.viewDidLoad()
-
-    // Do any additional setup after loading the view.
+    configureTableView()
   }
+
+  private func configureTableView() {
+    tableView.backgroundColor = .bkBackground
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+
+    tableView.delegate = self
+    tableView.dataSource = self
+    view.addSubview(tableView)
+
+    NSLayoutConstraint.activate([
+      tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+      tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+      tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+    ])
+  }
+
+}
+
+extension SettingsViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 3
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    switch indexPath.row {
+    case 0:
+      // App version
+      let cell = UITableViewCell()
+      var configuration = UIListContentConfiguration.cell()
+      cell.backgroundColor = .bkBackground
+      configuration.text = "앱 버전"
+      configuration.secondaryText = Versioning.appVersion
+      configuration.prefersSideBySideTextAndSecondaryText = true
+      cell.contentConfiguration = configuration
+      return cell
+    case 1:
+      return UITableViewCell()
+    case 2:
+      return UITableViewCell()
+    default:
+      return UITableViewCell()
+    }
+  }
+
+}
+
+extension SettingsViewController: UITableViewDelegate {
 
 }
