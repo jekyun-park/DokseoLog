4863bd6e8c6c805fd9a9d5d58b6547a28175de53
jegyun <parkjekyun@gmail.com>
2024-03-07 23:34:19 +0900

    :lipstick: Design: #43 - 수집한 문장이 없을 경우 안내 문구 추가
    내 책장 > 도서를 눌렀을 때 수집한 문장이 없는 경우 안내 문구를 보여줍니다.

--- DokseoLog/DokseoLog/Custom Views/ViewControllers/SentenceViewController.swift
+++ DokseoLog/DokseoLog/Custom Views/ViewControllers/SentenceViewController.swift
@@ -29,31 +29,56 @@ class SentenceViewController: UIViewController {
   let tableView = UITableView()
   var sentences = [Sentence]()
 
+  private lazy var emptyLabel: BKBodyLabel = {
+    let label = BKBodyLabel(textAlignment: .center, fontSize: 16, fontWeight: .medium)
+    label.text = "인상깊었던 문장을 추가해보세요."
+    return label
+  }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
     getSentences()
-    configureViewController()
-    configureTableView()
+    setupUI()
   }
 
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
-    sentences.removeAll()
     getSentences()
-    tableView.reloadData()
+    setupUI()
   }
 
   // MARK: Private
 
   private func getSentences() {
+    sentences.removeAll()
     let result = PersistenceManager.shared.fetchSentences(book)
-
     switch result {
     case .success(let results):
       sentences = results
     case .failure(let error):
       presentBKAlert(title: "저장된 문장을 불러올 수 없어요.", message: error.description, buttonTitle: "확인")
     }
+  }
+
+  private func setupUI() {
+    if sentences.isEmpty {
+      setupEmptyState()
+    } else {
+      configureTableView()
+      tableView.reloadData()
+    }
+    configureViewController()
+  }
+
+  private func setupEmptyState() {
+      view.addSubviews(emptyLabel)
+      NSLayoutConstraint.activate([
+        emptyLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+        emptyLabel.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+        emptyLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+        emptyLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+        emptyLabel.heightAnchor.constraint(equalToConstant: 24),
+      ])
   }
 
   private func configureViewController() {
